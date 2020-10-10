2b07adcbc3776f9190d99f272192f10a45b2f7c9
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-10-10 01:35:00 -0400

    added migration test

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -6,11 +6,26 @@ require("chai")
     .use(require("chai-as-promised"))
     .should()
 
+function tokens(n) {
+    return web3.utils.toWei(n, "Ether")
+}
+
 contract("TokenFarm", (accounts) => {
+
+    let daiToken, dappToken, tokenFarm
+    
+    before(async () => {
+        // Load Contracts
+        daiToken = await DaiToken.new()
+        dappToken = await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        // Transfer all Dapp tokens to farm (1 million)
+        await dappToken.transfer(tokenFarm.address, tokens("1000000"))
+    })
 
     describe("Mock DAI deployment", async () => {
         it("has a name", async () => {
-            let daiToken = await DaiToken.new()
             const name = await daiToken.name()
             assert.equal(name, "Mock DAI Token")
         })
